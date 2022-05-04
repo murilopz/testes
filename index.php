6d3182dfa94d8d2e1f318dd17dbb1dad4a8fc776
murilopz <89736634+murilopz@users.noreply.github.com>
2022-05-04 13:20:30 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$url = 'http://169.254.169.254/latest/meta-data/instance-id';
+
+$instance = file_get_contents($url);
+
+echo '<p>Instancia '.$instance;
+
+echo '<p>Remote Addr: '.$_SERVER['REMOTE_ADDR'];
+
+echo '<p>Server Addr: '.$_SERVER['SERVER_ADDR'];
+
+?>
