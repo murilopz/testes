e81f8fffcf35244a82db14c8ecbb7e688ce7a76f
murilopz <89736634+murilopz@users.noreply.github.com>
2022-05-09 16:26:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $url = 'http://169.254.169.254/latest/meta-data/instance-id';
 
 $instance = file_get_contents($url);
 
-echo '<p>Versao Github 7';
+echo '<p>Versao Github 8';
 
 echo '<p>Instancia '.$instance;
 
